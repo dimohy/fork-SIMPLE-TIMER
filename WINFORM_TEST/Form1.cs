5aedcb263c0b4714b36d921a6c7925de7bcd2af9
dimohy <58620778+dimohy@users.noreply.github.com>
2024-03-03 01:01:54 +0900

    수정

--- WINFORM_TEST/Form1.cs
+++ WINFORM_TEST/Form1.cs
@@ -1,9 +1,12 @@
 
+using System.Diagnostics;
+
 namespace WINFORM_TEST
 {
     public partial class Form1 : Form
     {
-        DateTime clock = new DateTime(0);
+        //DateTime clock = new DateTime(0);
+        Stopwatch clock = new Stopwatch();
         public Form1()
         {
 
@@ -27,9 +30,11 @@ namespace WINFORM_TEST
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            clock = clock.AddMilliseconds(100);
+            //clock = clock.AddMilliseconds(100);
             textBox2.Clear();
-            textBox2.AppendText($"{clock.Minute.ToString()}:{clock.Second.ToString()}:{clock.Millisecond.ToString()}");
+            //textBox2.AppendText($"{clock.Minute.ToString()}:{clock.Second.ToString()}:{clock.Millisecond.ToString()}");
+            var ts = clock.Elapsed;
+            textBox2.AppendText($"{ts.Minutes.ToString()}:{ts.Seconds.ToString()}:{ts.Milliseconds.ToString()}");
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -38,11 +43,13 @@ namespace WINFORM_TEST
             if (timer2.Enabled == false)
             {
                 timer2.Start();
+                clock.Restart();
                 button1.Text = "STOP";
             }
             else if (timer1.Enabled == true)
             {
                 timer2.Stop();
+                clock.Stop();
                 button1.Text = "START";
             }
 
@@ -50,14 +57,17 @@ namespace WINFORM_TEST
 
         private void button2_Click(object sender, EventArgs e)
         {
-            clock = clock.AddTicks(-clock.Ticks);
-            textBox2.Clear();
-            textBox2.AppendText($"{clock.Minute.ToString()}:{clock.Second.ToString()}:{clock.Millisecond.ToString()}");
+            //clock = clock.AddTicks(-clock.Ticks);
+            //textBox2.Clear();
+            //textBox2.AppendText($"{clock.Minute.ToString()}:{clock.Second.ToString()}:{clock.Millisecond.ToString()}");
+            clock.Restart();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox3.AppendText($"{DateTime.Now.ToString()} = {clock.Minute.ToString()}:{clock.Second.ToString()}:{clock.Millisecond.ToString()} {Environment.NewLine}");
+            //textBox3.AppendText($"{DateTime.Now.ToString()} = {clock.Minute.ToString()}:{clock.Second.ToString()}:{clock.Millisecond.ToString()} {Environment.NewLine}");
+            var ts = clock.Elapsed;
+            textBox3.AppendText($"{DateTime.Now.ToString()} = {ts.Minutes.ToString()}:{ts.Seconds.ToString()}:{ts.Milliseconds.ToString()} {Environment.NewLine}");
         }
 
         private void button4_Click(object sender, EventArgs e)
